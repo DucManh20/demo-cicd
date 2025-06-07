77e65bb067968650d6f26b5f9949ed6837297fdb
Phạm Đức Mạnh <phamducmanh@192.168.1.32.non-exists.ptr.local>
2025-06-08 00:16:40 +0700

    fix: unit test app controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! Demo CICD"', () => {
+      expect(appController.getHello()).toBe('Hello World! Demo CICD');
     });
   });
 });
